e99591be0f8c4114b23c67ced16d38656167739f
Max <bomax1597@gmail.com>
2023-11-30 20:48:29 +0100

    comments

--- notebooks/Graph.py
+++ notebooks/Graph.py
@@ -9,10 +9,12 @@ class Graph:
         self.nodes = [i for i in range(1, len(antenna_coordinates) + 1)]
         self.edges = []
         self.coordintes = antenna_coordinates
+        # Our graph will be stored here
         self.graph = None
         self.color = 0
         self.update_nx_graph()
         N = len(antenna_coordinates)
+        # We check the distance between every antenna and add an edge if the distance is < to the threshold
         for i in range(N):
             for j in range(N):
                 if i == j:
@@ -37,6 +39,7 @@ class Graph:
             self.graph.add_edge(n1, n2)
 
     def def_colors(self, bitstring):
+        # we associate the index of the colors to the corresponding node
         for i in range(len(bitstring)):
             if bitstring[i] == '1':
                 self.colors[i + 1] = list(Colors)[self.color].value
@@ -45,13 +48,16 @@ class Graph:
 
     def show_graph(self, pos):
         node_colors = [self.colors.get(node, 'lavender') for node in self.graph.nodes()]
-        nx.draw(self.graph, pos, with_labels=True, font_weight='bold', node_color=node_colors, font_color='black', font_size=8)
+        nx.draw(self.graph, pos, with_labels=True, font_weight='bold', node_color=node_colors, font_color='black',
+                font_size=8)
         plt.figure(1)
         plt.title("Graph")
         plt.axis('off')
         plt.show()
 
     def return_uncolored_graph(self):
+        # In this method we create a new graph from the old one but without any colors
+        # useful to itterate and apply the MIS function on smaller graphs
         new_graph = self.graph.copy()
         colored_nodes = [idx for idx in self.colors.keys()]
         new_graph.remove_nodes_from(colored_nodes)
